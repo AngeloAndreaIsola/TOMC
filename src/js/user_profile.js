97a6522db1be2143578d263674ae4d4e29ab308e
Angelo Andrea Isola <andrea.isola@me.com>
2021-11-24 19:47:26 +0100

    implementato profilo utente

---
+++ src/js/user_profile.js
@@ -0,0 +1,38 @@
+data = JSON.parse(localStorage["utente"])
+
+$('document').ready(function () {
+    console.log("Document ready!");
+    console.log("Local storage: " + JSON.parse(localStorage["utente"]));
+
+
+    document.getElementById("profileName").innerHTML = data.name;
+    document.getElementById("profileLastname").innerHTML = data.lastname;
+    document.getElementById("profileEmail").innerHTML = data.email;
+
+
+})
+
+function modificaNome() {
+    var newNome = prompt("Please enter your name");
+    if (newNome != null || newNome == "" || newNome == "") {
+        document.getElementById("profileName").innerHTML = newNome
+
+        var data = JSON.parse(localStorage["utente"])
+        data.name = newNome
+        localStorage.setItem('utente', JSON.stringify(data));
+
+    }  
+}
+
+//TODO: Non far modificare campo quando è nullo o withespace
+function modificaCognome() {
+    var newCognome = prompt("Please enter your name");
+    if (newCognome != null || newCognome == "" || newCognome == "") {
+        document.getElementById("profileLastname").innerHTML = newCognome
+
+        var data = JSON.parse(localStorage["utente"])
+        data.lastname = newCognome
+        localStorage.setItem('utente', JSON.stringify(data));
+
+    }  
+}
