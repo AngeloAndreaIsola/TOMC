8765d8a57fd695de083cb2d6884e3c1f3451d043
Angelo Andrea Isola <andrea.isola@me.com>
2021-12-29 21:14:14 +0100

    ricerca funzionante

--- src/js/shop_home.js
+++ src/js/shop_home.js
@@ -5,6 +5,7 @@ const BASE_URL_SEARCH ='https://api.themoviedb.org/3/search/movie'
 const API_URL = BASE_URL + "550" + API_KEY
 const searchURL = BASE_URL + '/search/movie'+API_KEY;
 const container= document.getElementById("palinsesto");
+const risultatoRicerca= document.getElementById("risultati_ricerca");
 const form =  document.getElementById('addMovieForm');
 const search = document.getElementById('search');
 var data;
@@ -36,9 +37,15 @@ $('document').ready(function () {
     //richiesta ricerca
     form.addEventListener('submit', (e) => {
         e.preventDefault();
+
+        //pulisce risultati vecchi
+        document.getElementById('risultati_ricerca').innerHTML = "";
     
-        searchMovie(search.value, res => {
-            console.log(res);
+        searchMovie(search.value, response => {
+            console.log(response);
+            response.results.forEach(element => {
+                displaySearchResult(element, element.id, risultatoRicerca)
+            })
         })
     
     })
@@ -100,7 +107,7 @@ function getMoviePoster(path, callback) {
     })
 }
 
-//data è opzionae
+
 function displayPalinsesto(result, idx, container, date) {
     date = date || 0;
     const card = document.createElement('div');
@@ -156,4 +163,46 @@ function displayPalinsesto(result, idx, container, date) {
         //console.log(entry.name);
         $("#genresN-" + idx).append(entry.name + ' ');
     })
+}  
+
+function displaySearchResult(result, idx, container) {
+    const card = document.createElement('div');
+    card.classList = 'card-body';
+
+    // Construct card content
+    const content = `
+  <div class="card">
+  <div class="crop">
+  <img id="moviePoster" src="${BASE_URL_IMG + result.backdrop_path}" style="width=200px"></img>
+  </div>
+  <div class="card-header" id="heading-${idx}">
+    <h5 class="mb-0">
+      <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
+
+              </button>
+    </h5>
+  </div>
+
+  <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
+    <div class="card-body">
+
+      <h5>${result.title}</h5>
+      <p id ="genres-${idx}"></p>
+      <p>${result.vote_average}</p>
+      <p>${result.release_date}</p>
+      <p id="demo-${idx}"></p>
+      <p>Prezzo: 10€</p>
+
+      <button id="shopAddMovie-${idx}" onclick=""><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+      <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+      <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+    </svg></button>
+    </div>
+  </div>
+</div>
+`;
+
+    // Append newyly created card element to the container
+    container.innerHTML += content;
+
 }  
