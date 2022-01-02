3aa78a3a850694f91b9fd4513f87af6cc7fed373
Angelo Andrea Isola <andrea.isola@me.com>
2022-01-02 15:11:07 +0100

    apre link nella stessa scheda

--- src/js/libreria.js
+++ src/js/libreria.js
@@ -99,7 +99,7 @@ function displayMovieN(result, idx, container, date) {
       <p>${result.vote_average}</p>
       <p>${result.release_date}</p>
       <p id="demo-${idx}"></p>
-      <button id="playRent-${idx}"><a href="data/logoIntro.mp4?id=${idx}"" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+      <button id="playRent-${idx}"><a href="data/logoIntro.mp4?id=${idx}"" target="_self"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
     </svg></button>
@@ -144,7 +144,7 @@ function displayMovieC(result, idx, container) {
       <p id="libGenresC-${idx}"></p>
       <p>${result.vote_average}</p>
       <p>${result.release_date}</p>
-      <button onLCick="play(${idx})"> <a href="data/logoIntro.mp4?id=${idx}"" target="_blank"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
+      <button onLCick="play(${idx})"> <a href="data/logoIntro.mp4?id=${idx}"" target="_self"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
       <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
       <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
     </svg></button>
@@ -195,8 +195,4 @@ function displayTimer(date, idx) {
             }
         }
     }, 1000);
-}
-
-function play(){
-
 }
