de3c58fac2017767ebb9270253ca05581dd0fd33
Angelo Andrea Isola <andrea.isola@me.com>
2022-01-02 11:32:55 +0100

    apre dettagli film in un altra scheda

--- src/js/movieInfo.js
+++ src/js/movieInfo.js
@@ -1,6 +1,7 @@
 const API_KEY = '?api_key=18cad5ee1c5382a869938ad511f2f321'
 const BASE_URL = 'https://api.themoviedb.org/3/movie/'
 const BASE_URL_IMG = 'https://image.tmdb.org/t/p/w500/'
+const BASE_URL_GET_CREDITS = 'https://api.themoviedb.org/3/movie/550/credits'
 const API_URL = BASE_URL + "550" + API_KEY
 
 
@@ -18,18 +19,47 @@ $('document').ready(function () {
     getMovie(id,response=>{
         console.log(response);
         $("#title").append(response.title)
-        $("#generes").append(response.genres)
-        $("#actors").append(response.actors)
+
+        response.genres.forEach(element => {
+            $("#generes").append(element.name)
+            $("#generes").append(" ")
+        });
+
+
         $("#score").append(response.vote_average)
-        $("#synopsis")
+        $("#synopsis").append(response.overview)
 
         getMoviePoster(response.backdrop_path, res => {
             $("#cover").attr('src',  BASE_URL_IMG + response.backdrop_path)
+        })
+
+        getActors(id, e => {
+            console.log("Attori:");
+            console.log(e);
+
+            e.cast.forEach(element => {
+                $("#actors").append(element.name)
+                $("#actors").append(" ")
+
+            });
+            
         })
     })
 
 
 })
+
+function getActors(code, callback) {
+    $.ajax({
+      type: "GET",
+      url: BASE_URL + code + '/credits'+ API_KEY,
+      data: JSON.stringify({}),
+      success: callback,
+      error: function (error) {
+        console.log(error.responseText);
+      }
+    })
+  }
 
 function getMovie(code, callback) {
     $.ajax({
