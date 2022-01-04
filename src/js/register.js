7e5a822165457feb34fa38979166acb4f74b79e9
Angelo Andrea Isola <andrea.isola@me.com>
2022-01-04 22:18:48 +0100

    risolto bug register

--- src/js/register.js
+++ src/js/register.js
@@ -29,48 +29,55 @@ function registerUser() {
         palinsesto: [],
         vendite: [],
         partitaIVA: "",
-        shopName:""
+        shopName: ""
     }
 
     //Controllo su input
+    var validName = true;
+    var validPsw = true;
+    
     for (var i = 0; i < data.length; i++) {
         // check if new username is equal to any already created usernames
         if (registerEmail == data[i].email) {
             // alert user that the username is take
             alert('That email is alreat in user, please choose another')
             // stop the statement if result is found true
+            validName = false
             break
             // check if new password is 8 characters or more
-        } else if (registerPassword.length < 3) {
-            // alert user that the password is to short
-            alert('That is to short, include 3 or more characters')
-            // stop the statement if result is found true
-            break
-        } else {
-            if (type == 'utente' && registerEmail != data[i].email) {
-                newUser.email = registerEmail
-                newUser.name = registerNome
-                newUser.lastname = registerCognome
-                newUser.password = registerPassword
-                newUser.type = type
-
-                data.push(newUser)
-                localStorage.setItem('data', JSON.stringify(data));
-                localStorage.setItem('utente', JSON.stringify(newUser));
-                window.location.replace("./user_home.html");
-            } else {
-                newShop.email = registerEmail
-                newShop.name = registerNome
-                newShop.lastname = registerCognome
-                newShop.password = registerPassword
-                newShop.type = type
-
-                data.push(newShop)
-                localStorage.setItem('data', JSON.stringify(data));
-                localStorage.setItem('utente', JSON.stringify(newShop));
-                window.location.replace("./home_shop.html");
-            }
         }
+    }
+
+
+    if (registerPassword.length < 3) {
+        // alert user that the password is to short
+        alert('That is to short, include 3 or more characters')
+        // stop the statement if result is found true
+        validPsw = false
+    }
+
+    if (type == 'utente' && validName == true && validPsw == true) {
+        newUser.email = registerEmail
+        newUser.name = registerNome
+        newUser.lastname = registerCognome
+        newUser.password = registerPassword
+        newUser.type = type
+
+        data.push(newUser)
+        localStorage.setItem('data', JSON.stringify(data));
+        localStorage.setItem('utente', JSON.stringify(newUser));
+        window.location.replace("./user_home.html");
+    } else if((type == 'shop' && validName == true && validPsw == true)) {
+        newShop.email = registerEmail
+        newShop.name = registerNome
+        newShop.lastname = registerCognome
+        newShop.password = registerPassword
+        newShop.type = type
+
+        data.push(newShop)
+        localStorage.setItem('data', JSON.stringify(data));
+        localStorage.setItem('utente', JSON.stringify(newShop));
+        window.location.replace("./home_shop.html");
     }
 }
 
