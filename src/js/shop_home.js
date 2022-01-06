bd5f1c962b7dd09aa7aed2b4e8136ae1269563e9
Angelo Andrea Isola <andrea.isola@me.com>
2022-01-06 15:38:29 +0100

    Modificato CSS

--- src/js/shop_home.js
+++ src/js/shop_home.js
@@ -161,7 +161,7 @@ function displayPalinsesto(result, idx, container, date, rentPrice, buyPrice) {
       <p>${result.release_date}</p>
       <p id="demo-${idx}"></p>
       <p id="buyPrice-${idx}">prezzo acquisto: ${buyPrice/100}</p>
-      <button onClick="modifyBuyPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+      <button class="btn btn-primary" onClick="modifyBuyPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
       class="bi bi-pencil-square" viewBox="0 0 16 16">
       <path
         d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -171,7 +171,7 @@ function displayPalinsesto(result, idx, container, date, rentPrice, buyPrice) {
 
     
     <p id="rentPrice-${idx}">prezzo noleggio: ${rentPrice/100}</p>
-    <button onClick="modifyRentPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+    <button class="btn btn-primary" onClick="modifyRentPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
     class="bi bi-pencil-square" viewBox="0 0 16 16">
     <path
       d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
@@ -180,7 +180,7 @@ function displayPalinsesto(result, idx, container, date, rentPrice, buyPrice) {
   </svg></button>
 
   <p>riuomvi da palisesto</p>
-  <button id="remove-${idx}" onclick="removeMovieFromPalinsesto(${idx})">
+  <button class="btn btn-danger" id="remove-${idx}" onclick="removeMovieFromPalinsesto(${idx})">
     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
   <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg></button>
@@ -229,7 +229,7 @@ function displaySearchResult(result, idx, container) {
       <p id="demo-${idx}"></p>
       <p>Prezzo: 10€</p>
 
-      <button id="shopAddMovieToCart-${idx}" onclick="addMovieToCart(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+      <button class="btn btn-danger" id="shopAddMovieToCart-${idx}" onclick="addMovieToCart(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
       <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
       <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
     </svg></button>
