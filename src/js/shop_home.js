0c91ee3d82a915f1ca80c8a29ece88130a12b7f6
Angelo Andrea Isola <andrea.isola@me.com>
2022-01-07 14:31:12 +0100

    cambiato grafica card shop

--- src/js/shop_home.js
+++ src/js/shop_home.js
@@ -23,7 +23,7 @@ $('document').ready(function () {
   //mostra palinsesto
   palinsesto.forEach(element => {
     getMovie(element.id, (response) => {
-      console.log(response);
+      //console.log(response);
       displayPalinsesto(response, response.id, container, element.date, element.priceRent, element.price)
       getMoviePoster(response.poster_path, (responseBis) => {
         $('#moviePoster').append(responseBis)
@@ -141,50 +141,42 @@ function displayPalinsesto(result, idx, container, date, rentPrice, buyPrice) {
 
   // Construct card content
   const content = `
-  <div class="card" id="card-${idx}">
-  <div class="crop">
-  <img id="moviePoster" src="${BASE_URL_IMG + result.poster_path}"  style="width=200px"></img>
-  </div>
-  <div class="card-header" id="heading-${idx}">
-    <h5 class="mb-0">
-      <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
-
-              </button>
-    </h5>
-  </div>
-
-  <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
-    <div class="card-body">
-
-      <h5>${result.title}</h5>
-      <p id="demo-${idx}"></p>
-      <p id="buyPrice-${idx}">prezzo acquisto: ${buyPrice/100}</p>
-      <button class="btn btn-primary" onClick="modifyBuyPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-      class="bi bi-pencil-square" viewBox="0 0 16 16">
-      <path
-        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-      <path fill-rule="evenodd"
-        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-    </svg></button>
-
-    
-    <p id="rentPrice-${idx}">prezzo noleggio: ${rentPrice/100}</p>
-    <button class="btn btn-primary" onClick="modifyRentPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-    class="bi bi-pencil-square" viewBox="0 0 16 16">
-    <path
-      d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-    <path fill-rule="evenodd"
-      d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-  </svg></button>
+</div>
+<div class="card movie_card">
+<img src="${BASE_URL_IMG + result.poster_path}" class="card-img-top" alt="...">
+<div class="card-body">
+<h5 class="card-title">${result.title}</h5>
+   <span class="movie_info" id="date-${idx}">${new Date(result.release_date).getFullYear()}</span>
+   <span class="movie_info float-right"> ${result.vote_average}</span>
 
 
-  <button class="btn btn-danger" id="remove-${idx}" onclick="removeMovieFromPalinsesto(${idx})">
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
-  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+   <p id="buyPrice-${idx}">prezzo acquisto: ${buyPrice/100}</p>
+   <button class="btn btn-primary" onClick="modifyBuyPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+   class="bi bi-pencil-square" viewBox="0 0 16 16">
+   <path
+     d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+   <path fill-rule="evenodd"
+     d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+ </svg></button>
+
+ 
+ <p id="rentPrice-${idx}">prezzo noleggio: ${rentPrice/100}</p>
+ <button class="btn btn-primary" onClick="modifyRentPrice(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+ class="bi bi-pencil-square" viewBox="0 0 16 16">
+ <path
+   d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+ <path fill-rule="evenodd"
+   d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+</svg></button>
+
+<p></p>
+
+<button class="btn btn-danger btn-block" id="remove-${idx}" onclick="removeMovieFromPalinsesto(${idx})">
+ <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x" viewBox="0 0 16 16">
+<path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
 </svg>Rimuovi</button>
 
-    </div>
-  </div>
+</div>
 </div>
 `;
 
@@ -200,34 +192,21 @@ function displaySearchResult(result, idx, container) {
 
   // Construct card content
   const content = `
-  <div class="card">
-  <div class="crop">
-  <img id="moviePoster" src="${BASE_URL_IMG + result.poster_path}" style="width=200px"></img>
-  </div>
-  <div class="card-header" id="heading-${idx}">
-    <h5 class="mb-0">
-      <button class="btn btn-link" data-toggle="collapse" data-target="#collapse-${idx}" aria-expanded="true" aria-controls="collapse-${idx}">
+</div>
+<div class="card movie_card">
+<img src="${BASE_URL_IMG + result.poster_path}" class="card-img-top" alt="...">
+<div class="card-body">
+<h5 class="card-title">${result.title}</h5>
+   <span class="movie_info" id="date-${idx}">${new Date(result.release_date).getFullYear()}</span>
+   <span class="movie_info float-right"> ${result.vote_average}</span>
 
-              </button>
-    </h5>
-  </div>
+    <p></p>
 
-  <div id="collapse-${idx}" class="collapse show" aria-labelledby="heading-${idx}" data-parent="#accordion">
-    <div class="card-body">
-
-      <h5>${result.title}</h5>
-      <p id ="genres-${idx}"></p>
-      <p>${result.vote_average}</p>
-      <p>${result.release_date}</p>
-      <p id="demo-${idx}"></p>
-      <p>Prezzo: 10€</p>
-
-      <button class="btn btn-success" id="shopAddMovieToCart-${idx}" onclick="addMovieToCart(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
-      <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
-      <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
-    </svg></button>
-    </div>
-  </div>
+   <button class="btn btn-success btn-block" id="shopAddMovieToCart-${idx}" onclick="addMovieToCart(${idx})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart-plus" viewBox="0 0 16 16">
+   <path d="M9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z"/>
+   <path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zm3.915 10L3.102 4h10.796l-1.313 7h-8.17zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0z"/>
+ </svg></button>
+</div>
 </div>
 `;
 
